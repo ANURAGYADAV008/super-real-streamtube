3a10458cc538a85bc444c3abcbe5435a89dbada8
Anurag Yadav <jaysinghanurag321@gmail.com>
2025-12-07 12:55:21 +0530

    Work

--- src/Components/Videcontainer.jsx
+++ src/Components/Videcontainer.jsx
@@ -14,7 +14,7 @@ const VideoContainer = () => {
     const handleYoutubevideo = async () => {
 
             try {
-                const response = await fetch("https://super-real-streamtube-production.up.railway.app/api/v1/youtube");
+                const response = await fetch("https://super-real-streamtube-production.up.railway.app/api/v1/youtube/getvideo");
                 const data = await response.json();
                 console.log(data?.video.items)
                 setVideo(data?.video?.items);
